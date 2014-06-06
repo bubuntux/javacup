3ff9295684740bc9f260fd1c746bc9137abdd11e
Julio Gutierrez <bubuntux@gmail.com>
2014-06-05 22:42:07 -0500

    match screen

--- core/src/main/java/org/dsaw/javacup/screen/MatchScreen.java
+++ core/src/main/java/org/dsaw/javacup/screen/MatchScreen.java
@@ -2,14 +2,41 @@ package org.dsaw.javacup.screen;
 
 import com.badlogic.gdx.Screen;
 
+import org.dsaw.javacup.JavaCup;
+import org.dsaw.javacup.model.engine.StoredMatch;
+
 /**
  * @author Julio Gutierrez (29/05/2014)
  */
 public class MatchScreen implements Screen {
 
+
+  private final JavaCup game;
+  private final StoredMatch match;
+  private final int width;
+  private final int height;
+
+  public MatchScreen(JavaCup game, StoredMatch match, int width, int height) {
+    this.game = game;
+    this.match = match;
+    this.width = width;
+    this.height = height;
+
+
+  }
+
+  private void update(float delta) {
+
+  }
+
+  private void draw() {
+
+  }
+
   @Override
   public void render(float delta) {
-
+    update(delta);
+    draw();
   }
 
   @Override
